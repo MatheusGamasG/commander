1c4b20bcce34cfd513a96d646469f163a2c161ac
Matheus Guimarães <mgguima95@gmail.com>
2023-02-07 20:48:09 -0300

    fix: removing unused changes

--- commander_backend/src/main/java/br/com/commander_backend/constants/ApiConstants.java
+++ commander_backend/src/main/java/br/com/commander_backend/constants/ApiConstants.java
@@ -3,6 +3,5 @@ package br.com.commander_backend.constants;
 public class ApiConstants {
 
     public static final String COLLECTION_NAME = "Game";
-    public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
 }
